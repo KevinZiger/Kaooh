8da91868f95ea031cd4a0bf07a627e320fa2ce35
kevinziger <k901212k@126.com>
2014-12-08 15:40:10 +0800

    add db

--- app/src/main/java/com/ziger/kaooh/activities/MainActivity.java
+++ app/src/main/java/com/ziger/kaooh/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.ziger.kaooh;
+package com.ziger.kaooh.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ziger.kaooh.R;
 
 
 public class MainActivity extends Activity {
---
+++ app/src/main/java/com/ziger/kaooh/model/Province.java
@@ -0,0 +1,34 @@
+package com.ziger.kaooh.model;
+
+/**
+ * Created by kaka on 2014/12/8.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
